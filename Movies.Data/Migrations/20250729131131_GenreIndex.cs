4f3b9c70052a9b4345553e95fbf8d53112ac7e18
Anna Eklund <anna.eklund@yahoo.se>
2025-07-29 15:12:11 +0200

    Add genre index migration

---
+++ Movies.Data/Migrations/20250729131131_GenreIndex.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenreIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Genre_Name",
+                table: "Genre",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UQ_Genre_Name",
+                table: "Genre");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
