fe1e79004f38c3cdd51ea4795b97a00801aeda6b
Anna Eklund <anna.eklund@yahoo.se>
2025-08-02 16:06:20 +0200

    Add fix

--- Movies.Presentation/Controllers/ReviewsController.cs
+++ Movies.Presentation/Controllers/ReviewsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Movies.Contracts;
 using Movies.Core.Domain.Models.DTOs.ReviewDtos;
